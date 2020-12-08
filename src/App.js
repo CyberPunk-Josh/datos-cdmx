418933a414cfc89f10b778f2a952d2a47aba7218
Joshue <cristianjoshue@hotmail.com>
2020-12-07 19:43:14 -0600

    proyecto casi terminado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [resultado, guardarResultado] = useState({});
+
+
+  useEffect( () => {
+    const consultarApi = async () =>{
+      
+      const url = 'https://datos.cdmx.gob.mx/explore/embed/dataset/carpetas-de-investigacion-pgj-de-la-ciudad-de-mexico/map/?disjunctive.ao_hechos&disjunctive.delito&location=10,19.40184,-98.9669&static=false&datasetcard=false&scrollWheelZoom=false" width="400" height="300" frameborder="0"'
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      // console.log(resultado);
+
+      guardarResultado(resultado);
+    }
+    consultarApi();
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1 className='text-center'>Datos CDMX</h1>
+
+      <iframe>
+          
+      </iframe>
+    </Fragment>
   );
 }
 
